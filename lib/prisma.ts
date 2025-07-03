7ac74144051daf3cc67822a74a356b5bc7bb95c1
Muhammad Uzair <muhammad.druzair@gmail.com>
2025-07-03 16:20:13 +0500

    prism fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/generated/client"; // adjust based on your project structure
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
